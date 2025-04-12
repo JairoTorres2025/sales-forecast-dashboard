c9182036753e1673a5a44664cfcee8120462a75b
vishal9-code <vishalprajapati2549@gmail.com>
2025-04-12 22:22:59 +0530

    Update streamlit_app_py.py

--- streamlit_app_py.py
+++ streamlit_app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1xauanPiPhALKGyrq7Q_LBzpeRuuOJLpp
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
